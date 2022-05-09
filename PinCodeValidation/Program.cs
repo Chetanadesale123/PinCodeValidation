168e6564bc386202721cfe5643fdea706da9a420
59114_Chetana <deslechetna1999@gmail.com>
2022-05-09 16:59:13 +0530

    [Chetana] Added : Rigester Pin Code

--- PinCodeValidation/Program.cs
+++ PinCodeValidation/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PinCodeValidation;
+using System;
+class Program
+{
+    static void Main(String[] args)
+    {
+        RegisterPinCode valid = new RegisterPinCode();
+        // valid.CheckPin("400088");
+        //valid.CheckPin("A400088");
+        valid.CheckPin("400088B");
+    }
+}
---
+++ PinCodeValidation/RegisterPinCode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PinCodeValidation
+{
+    public class RegisterPinCode
+    {
+        //public const string PINCODE_REGEX = "^[0-9]{6,}$";
+        // public const string PINCODE_REGEX = "^[A-Z]{1}[0-9]{6}$";
+        public const string PINCODE_REGEX = "^[0-9]{6}[A-Z]{1}$";
+        public void CheckPin(string pincode)
+        {
+            if (Regex.IsMatch(pincode, PINCODE_REGEX))
+            {
+                Console.WriteLine("pincode matches");
+            }
+            else
+            {
+                Console.WriteLine("verify pincode again");
+            }
+        }
+    }
+}
+
+
